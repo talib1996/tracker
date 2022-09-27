d11247372be6a9c50f03b367a70c15e4e85bc781
Talib <fahadimtiaz46@gmail.com>
2022-09-27 22:45:34 +0500

    changed database credentials from local to remote

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
 //Database settings
-define('DRIVER', 'mysql');
-define('HOST', '127.0.0.1');
-define('PORT', '3306');
-define('USER', 'root');
-define('PASSWORD', '');
-define('DATABASE', 'tracker');
+define('DRIVER', 'pgsql');
+define('HOST', 'ec2-34-193-44-192.compute-1.amazonaws.com');
+define('PORT', '5432');
+define('USER', 'qhjhzfzjzgraho');
+define('PASSWORD', '0c5e9b7166ada46f5c31b4e882345ac5f650f9874864a098821fce0b9b840c3c');
+define('DATABASE', 'deqlpgrg1lvgtf');
