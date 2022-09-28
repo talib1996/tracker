f04c5f0fda36e37c3d783fbbeac5aae02d19d178
Talib <fahadimtiaz46@gmail.com>
2022-09-28 12:43:59 +0500

    changed config files

--- config/database.php
+++ config/database.php
@@ -1,29 +1,8 @@
 <?php
-//Database settings
-<<<<<<< HEAD
-// define('DRIVER', 'pgsql');
-// define('HOST', '127.0.0.1');
-// define('PORT', '5432');
-// define('USER', 'postgres');
-// define('PASSWORD', 'postgres');
-// define('DATABASE', 'tracker');
-
-//Database settings
-// define('DRIVER', 'mysql');
-// define('HOST', '127.0.0.1');
-// define('PORT', '3306');
-// define('USER', 'root');
-// define('PASSWORD', '');
-// define('DATABASE', 'tracker');
-
-//Database settings
-define('DRIVER', 'pgsql');
-=======
+// Database settings
 define('DRIVER', 'mysql');
->>>>>>> cff6b80 (changed a file)
 define('HOST', '127.0.0.1');
 define('PORT', '3306');
 define('USER', 'root');
 define('PASSWORD', '');
 define('DATABASE', 'tracker');
-
