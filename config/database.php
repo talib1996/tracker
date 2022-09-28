ddd25bc733160369b64f54aad8a523e4e728ca3b
M.Fahad Imtiaz <50912897+talib1996@users.noreply.github.com>
2022-09-28 12:17:24 +0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 //Database settings
 define('DRIVER', 'pgsql');
 define('HOST', '127.0.0.1');
-define('PORT', '5432');
-define('USER', 'postgres');
-define('PASSWORD', 'postgres');
+define('PORT', '3306');
+define('USER', 'root');
+define('PASSWORD', '');
 define('DATABASE', 'tracker');
